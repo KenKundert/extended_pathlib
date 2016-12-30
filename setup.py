35241b931940baf2ac868d237a3a9abca0b0743b
Ken Kundert <thenurd@nurdletech.com>
2016-12-29 19:04:19 -0800

    Stop including old pathlib library on modern versions of python.

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ except ImportError:
 with open('README.rst') as f:
     readme = f.read()
 
+from sys import version_info
+
+if version_info < '3.4':
+    requirements=['pathlib', 'six']
+else:
+    requirements=['six']
+
 setup(
     name='extended_pathlib',
     version='0.3.1',
@@ -18,11 +25,8 @@ setup(
     download_url='https://github.com/kenkundert/extended_pathlib/tarball/master',
     license='GPLv3+',
     zip_safe=True,
-    install_requires=['pathlib', 'six'],
-    keywords=[
-        'extended',
-        'pathlib',
-    ],
+    install_requires=requirements,
+    keywords=['extended', 'pathlib'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
